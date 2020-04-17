74c924c7ab2a80e69d7a6d9e9f81f37254bed4cf
TAEHOONS <63626900+TAEHOONS@users.noreply.github.com>
2020-04-17 13:21:39 +0900

    1시20분

--- app/src/main/java/com/example/baily/MainActivity.java
+++ app/src/main/java/com/example/baily/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.baily;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button gotoRegisterPage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        gotoRegisterPage = (Button)findViewById(R.id.lp_logJoin);
+        gotoRegisterPage.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent01 = new Intent(MainActivity.this, RegisterPage.class);
+        startActivity(intent01);
     }
 }
