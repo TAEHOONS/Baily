bcae649d26cfaf0c5844bf55b2cafde3b41d66ca
tjdqlsdl5456 <59725671+tjdqlsdl5456@users.noreply.github.com>
2020-04-24 12:45:14 +0900

    메인 통합본

---
+++ app/src/main/java/com/example/baily/FragHome.java
@@ -0,0 +1,27 @@
+package com.example.baily;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class FragHome extends Fragment {
+    private View view;
+
+    public static FragHome newInstance(){
+        FragHome fragHome = new FragHome();
+        return fragHome;
+    }
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.frag_home, container, false);
+        return view;
+    }
+}
